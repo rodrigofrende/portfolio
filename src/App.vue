827cfdf4da52b2f293735ad20935cd49167500dd
rodrigofrende <rodrigo.frende@gmail.com>
2021-08-09 19:22:41 -0300

    start working on header, nav links , & general styles and guidelines

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,67 @@
 <template>
-  <div class="p-14 bg-white dark:bg-black">
-    <h1 class=" text-yellow-400 flex justify-center">R F</h1>
-    <button class="text-white-300 bg-gray-300 px-4 py-2 flex justify-center rounded-xl hover:opacity-90 dark:bg-blue-400 " @click="toggleTheme" type="button">{{themeText}}</button>
-    <router-link class="  " to="/"></router-link>
+  <div
+    class="
+      container
+      mx-auto
+      h-screen
+      p-10
+      rounded-lg
+      transition-all
+      duration-500
+      bg-blue-light
+      dark:bg-blue-dark
+    "
+  >
+    <Header :themeText="themeText"/>
+    <div class="grid grid-cols-12">
+      <div class="col-span-12">
+        <h1 class="text-yellow-400 flex justify-center">R F</h1>
+        <button
+          class="
+            font-bold
+            bg-blue-light
+            text-black
+            border-2
+            px-4
+            py-2
+            justify-center
+            rounded-xl
+            hover:
+            dark:bg-blue-dark dark:text-white
+          "
+          @click="toggleTheme"
+          type="button"
+        >
+          {{ themeText }}
+        </button>
+      </div>
+    </div>
+    <router-link class="" to="/"></router-link>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import Header from "@/components/Header";
 export default {
-  data () {
-    return {
-    }
+  components: {
+    Header,
+  },
+  data() {
+    return {};
   },
   computed: {
     ...mapGetters({ theme: "getTheme" }),
-    themeText () {
-      if (this.theme === 'light') {
-        return  'Shine on you'
+    themeText() {
+      if (this.theme === "light") {
+        return "Shine on you";
       } else {
-        return 'Getting Dark'
+        return "Getting Dark";
       }
-    }
+    },
   },
-  mounted () {
-    this.$store.dispatch('initTheme')
+  mounted() {
+    this.$store.dispatch("initTheme");
   },
   watch: {
     theme(newTheme) {
@@ -34,15 +71,14 @@ export default {
     },
   },
   methods: {
-    switchDarkMode () {
-      this.darkMode = !this.darkMode
+    switchDarkMode() {
+      this.darkMode = !this.darkMode;
     },
     toggleTheme() {
       this.$store.dispatch("toggleTheme");
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
-<style>
-</style>
+<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,30 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
-  darkMode: 'class', // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      "blue-dark": "#023047",
+      "blue-dark-hover": "#033249",
+      "blue-light": "#8ECAE6",
+      "header-bar": "#219EBC",
+      white: colors.white,
+      black: colors.black,
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [
-    plugin(function({ addBase, theme }) {
+    plugin(function ({ addBase, theme }) {
       addBase({
-        'h1': { fontSize: '40px' },
-        'h2': { fontSize: theme('fontSize.xl') },
-        'h3': { fontSize: theme('fontSize.lg') },
-      })
-    })
+        h1: { fontSize: "40px" },
+        h2: { fontSize: theme("fontSize.xl") },
+        h3: { fontSize: theme("fontSize.lg") },
+      });
+    }),
   ],
 };
